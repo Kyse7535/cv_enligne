d790e3dc77ddef30091e607a4772aac1a89fe4cb
Kyse7535 <ktevot@gmail.com>
2020-10-31 13:22:43 +0100

    cv enligne

---
+++ JS/index.js
@@ -0,0 +1,6 @@
+import { hamburger } from '../../../drop_down1/src/drop_down';
+$(document).ready(function() {
+    hamburger.add_hamburger();
+    hamburger.add_dropdown();
+    hamburger.toggle_hamburger();
+});
